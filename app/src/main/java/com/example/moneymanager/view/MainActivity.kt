9e8cc7764d274d87a18298eb39f2939cfdd5060f
angelairena14 <angela.irena@koinworks.com>
2020-10-21 10:44:10 +0700

    change fragment to activity

--- app/src/main/java/com/example/moneymanager/view/MainActivity.kt
+++ app/src/main/java/com/example/moneymanager/view/MainActivity.kt
@@ -2,20 +2,53 @@ package com.example.moneymanager.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.moneymanager.R
+import com.example.moneymanager.databinding.ActivityMainBinding
+import com.example.moneymanager.model.PostInfo
+import com.example.moneymanager.view.adapter.PostListAdapter
+import com.example.moneymanager.viewmodel.RetrofitViewModel
+import com.example.moneymanager.viewmodel.RetrofitViewModelFactory
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding: ActivityMainBinding
+    lateinit var retrofitViewModel: RetrofitViewModel
+    lateinit var postListAdapter: PostListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        loadFragment()
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        initViewModel()
+        initAdapter()
+        setAdapter()
+        fetchRetroInfo()
     }
 
-    fun loadFragment(){
-        var transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container_main,PostFragment())
-        transaction.addToBackStack(null)
-        transaction.commit()
+    override fun onDestroy() {
+        super.onDestroy()
+        retrofitViewModel.detatch()
+    }
+
+    fun initViewModel(){
+        var retrofitViewModelFactory = RetrofitViewModelFactory()
+        retrofitViewModel = ViewModelProviders.of(this,retrofitViewModelFactory).get(RetrofitViewModel::class.java)
+    }
+
+    fun initAdapter(){
+        postListAdapter = PostListAdapter(this)
+    }
+
+    fun setAdapter(){
+        binding.rvPostList.adapter = postListAdapter
+    }
+
+    fun fetchRetroInfo(){
+        retrofitViewModel.postInfoLiveData.observe(this,
+            Observer<List<PostInfo>> { t ->
+                t?.let {
+                    postListAdapter.setAdapterList(t)
+                }
+            })
     }
 }
